d95624aa12671a6bab1e01e9092ca65a8b4002cc
HACKSTYGIAN <HACKSTYGIAN@HACKSTYGIAN>
2023-03-16 13:49:35 +0800

    Quiz game

---
+++ Fun_Game/src/fGame/QuizGame.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package fGame;
+
+/**
+ * @author HACKSTYGIAN
+ *
+ */
+public class QuizGame {
+
+	/**
+	 * 
+	 */
+	public QuizGame() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Fun_Game/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HACKSTYGIAN
+ *
+ */
+module Fun_Game {
+}
